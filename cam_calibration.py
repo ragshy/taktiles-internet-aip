4495e58c271081a1dc27a73e91e7a48dcdff3b7f
sn00r <2097noor@gmail.com>
2022-12-05 19:56:15 +0100

    Prepared for pushing to main

--- cam_calibration.py
+++ cam_calibration.py
@@ -1,99 +1,97 @@
-import cv2
 import numpy as np
-import glob 
-import os 
-# script to yield the focal length, distortion and camera matrix
-# Trust Vero GTX camera: 8 Megapixels, Video Resolution 1920x1080, FOV: 55 degrees 
- 
-# 
-# Define the dimensions of checkerboard
-CHECKERBOARD = (8, 8)
- 
- 
-# stop the iteration when specified
-# accuracy, epsilon, is reached or
-# specified number of iterations are completed.
-criteria = (cv2.TERM_CRITERIA_EPS +
-            cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
- 
- 
-# Vector for 3D points
-threedpoints = []
- 
-# Vector for 2D points
-twodpoints = []
- 
- 
-#  3D points real world coordinates
-objectp3d = np.zeros((1, CHECKERBOARD[0]
-                      * CHECKERBOARD[1],
-                      3), np.float32)
-objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0],
-                               0:CHECKERBOARD[1]].T.reshape(-1, 2)
-prev_img_shape = None
- 
- 
-# Extracting path of individual image stored
-# in a given directory. Since no path is
-# specified, it will take current directory
-# jpg files alone
-images = glob.glob('*.png')
+import cv2
+import glob
+import yaml
 
-for filename in images:
-    image = cv2.imread('Chess_Board.svg.png')
-    grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    print(image)
-    # Find the chess board corners
-    # If desired number of corners are
-    # found in the image then ret = true
-    ret, corners = cv2.findChessboardCorners(
-                    grayColor, CHECKERBOARD,
-                    cv2.CALIB_CB_ADAPTIVE_THRESH
-                    + cv2.CALIB_CB_FAST_CHECK +
-                    cv2.CALIB_CB_NORMALIZE_IMAGE)
+# Issue: For loop not looping
 
-    # If desired number of corners can be detected then,
-    # refine the pixel coordinates and display
-    # them on the images of checker board
+corner_x=10 # number of chessboard corner in x direction
+corner_y=7 # number of chessboard corner in x direction
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.01)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((corner_y*corner_x,3), np.float32)
+objp[:,:2] = np.mgrid[0:corner_x,0:corner_y].T.reshape(-1,2)
+
+# Arrays to store object points and image points from a ll the images.
+objpoints = [] # 3d point in real world space
+jpegpoints = [] # 2d points in image plane.
+
+source_path = 'C:\\Users\\na86666\\Desktop\\taktiles-internet-aip\\callibration_data' 
+#print('images found :',len(os.listdir(source_path))) # count number of images
+
+# Create list with directories of each image    
+images = glob.glob(r'C:\Users\na86666\Desktop\taktiles-internet-aip\callibration_data\*.jpeg')
+#print(images)
+found = 0
+pat_directory = r'C:\Users\na86666\Desktop\taktiles-internet-aip\Patterned_images'
+
+for fname in images: # here, 10 can be changed to whatever number you like to choose
+    jpeg = cv2.imread(fname) # capture frame by frame
+    cv2.imshow('jpeg', jpeg)
+    cv2.waitKey(50)
+    #print(fname)
+    gray = cv2.cvtColor(jpeg, cv2.COLOR_BGRA2GRAY)
+    
+    # find the chess noard corners
+    ret, corners = cv2.findChessboardCorners(gray, (corner_x,corner_y), None)
+    # if found, ass object points, image points (after refining them)
     if ret == True:
-        threedpoints.append(objectp3d)
- 
-        # Refining pixel coordinates
-        # for given 2d points.
-        corners2 = cv2.cornerSubPix(
-            grayColor, corners, (11, 11), (-1, -1), criteria)
- 
- 
+        
+        objpoints.append(objp) #Certainly, every loop objp is the same in 3D
+        corners2 = cv2.cornerSubPix(gray,corners,(20,5),(-1,-5),criteria)
+        jpegpoints.append(corners2)
         # Draw and display the corners
-        image = cv2.drawChessboardCorners(image,
-                                          CHECKERBOARD,
-                                          corners2, ret)
- 
-    cv2.imshow('img', image)
-    cv2.waitKey(0)
- 
+        jpeg = cv2.drawChessboardCorners(jpeg, (corner_x,corner_y), corners2, ret)
+        found += 1
+        cv2.imshow('chessboard', jpeg)
+        cv2.waitKey(50)
+        #cv2.imwrite(str(pat_directory)+'\pat_img'+str(found)+'.jpeg', jpeg)
+print("Number of images used for calibration: ", found)
+
+ # when everything done, release the capture
+#cap.release()
 cv2.destroyAllWindows()
- 
-h, w = image.shape[:2]
- 
- 
-# Perform camera calibration by
-# passing the value of above found out 3D points (threedpoints)
-# and its corresponding pixel coordinates of the
-# detected corners (twodpoints)
-ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(
-    threedpoints, twodpoints, grayColor.shape[::-1], None, None)
- 
- 
-# Displaying required output
-print(" Camera matrix:")
-print(matrix)
- 
-print("\n Distortion coefficient:")
-print(distortion)
- 
-print("\n Rotation Vectors:")
-print(r_vecs)
- 
-print("\n Translation Vectors:")
-print(t_vecs)
+
+#calibration
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, jpegpoints, gray.shape[::-1], None, None)
+
+h, w = jpeg.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx, dist , (w,h), 1, (w,h))
+#undistort image
+undis_directory = r'C:\Users\na86666\Desktop\taktiles-internet-aip\Undistorted_images'
+undis_cropped_directory = r'C:\Users\na86666\Desktop\taktiles-internet-aip\Undistorted_cropped_images'
+found = 0
+for fname in images: # here, 10 can be changed to whatever number you like to choose
+     #print(fname)
+     jpeg = cv2.imread(fname) # Capture frame-by-frame
+     #cv2.imshow('jpeg', jpeg)
+     #cv2.waitkey(500)
+     found += 1
+     #undistort
+     dst = cv2.undistort(jpeg, mtx, dist, None, newcameramtx)
+     cv2.imshow('undistorted', dst)
+     cv2.waitKey(500)
+     #cv2.imwrite(str(undis_directory)+'\imdist_img'+str(found)+'.jpeg', dst)
+
+     # crop the image
+     x, y, w, h = roi
+     dst = dst[y:y+h, x:x+w]
+     #cv2.imshow('calibration.png',dst)
+     cv2.imshow('undistort_cropped', dst)
+     cv2.waitKey(500)
+     #cv2.imwrite(str(undis_cropped_directory)+'\imdist_cropped_img'+str(found)+'.jpeg', dst)
+     
+cv2.destroyAllWindows() 
+
+# transforms the matrix distortion coefficients to writeable lists
+data= {'camera_matrix': np.asarray(mtx).tolist(), 'new_camera_matrix': np.asarray(newcameramtx).tolist(), 'dist_coeff': np.asarray(dist).tolist()}
+print("Camera Matrix: ",mtx)
+print("Distortion Coefficients: ", dist)
+print("Optimized Camera Matrix: ", newcameramtx)
+# and save it to a file
+with open("calibration_matrix.yaml", "w")as f:
+    yaml.dump(data, f)
+
