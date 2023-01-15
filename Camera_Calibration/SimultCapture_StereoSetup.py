c53a5264f8a70006c0a40891a49c660054ba4e1c
sn00r <2097noor@gmail.com>
2023-01-15 19:15:31 +0100

    Lab two camera setup calibration

--- Camera_Calibration/SimultCapture_StereoSetup.py
+++ Camera_Calibration/SimultCapture_StereoSetup.py
@@ -31,8 +31,8 @@ while True:
         #img1_name = "cam1_frame_{}.png".format(img1_counter)
         cv2.imwrite(str(img0_counter)+'.png', frame0)
         cv2.imwrite(str(img1_counter)+'.png', frame1)
-        #print("{} written!".format(img0_name))
-        #print("{} written!".format(img1_name))
+        print("cam0 no.: ", img0_counter)
+        print("cam1 no.: ", img1_counter)
         img0_counter += 1
         img1_counter += 1
 
